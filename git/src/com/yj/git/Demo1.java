d4d21c6d46210f0c6b690ee6e92f39320d1bc6cc
yangjie220 <374712495@qq.com>
2019-10-18 14:39:12 +0800

    git send local respository

---
+++ git/src/com/yj/git/Demo1.java
@@ -0,0 +1,14 @@
+package com.yj.git;
+
+/**
+ * @author 仰望天空
+ * @site
+ * @company xxx公司
+ * @create 2019-10-18 14:36
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+    }
+
+}
